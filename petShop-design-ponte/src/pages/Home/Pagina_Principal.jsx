ca9a39f2d2f011a28ca979b9a2a4ef69558d29f0
Ademário de Sousa Neto <ademarionetoooo@gmail.com>
2025-04-01 20:03:10 -0300

    home

--- petShop-design-ponte/src/pages/Home/Pagina_Principal.jsx
+++ petShop-design-ponte/src/pages/Home/Pagina_Principal.jsx
@@ -1,20 +1,35 @@
-import './Home.css'
+import './Home.css';
 
 function Home() {
-  
   return (
-    <div className='pricipal'>
-      <h1>Olá, seja bem vindo(a), é um prazer ter você aqui! 🎉 <br />Agora você faz parte da nossa família pet, <br />onde oferecemos os melhores serviços para cuidar <br />do seu bichinho com carinho e dedicação. 🐶🐱✨</h1>
-      <br />
-      <h2>Agende já um dos nossos serviços para seu Pet</h2>
+    <div>
+      {/* barra de home inicio e cadastro */}
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Cadastro</a></li>
+        </ul>
+      </nav>
 
-      <br />
-      <br />
-      <br />
-      <br />
+      {/* organizacao */}
+      <div className="container">
+        <div className="content">
+          <img src="/lobo-removebg-preview.png" alt="Imagem de boas-vindas" />
+          <div className="text">
+            <h1>Olá, seja bem-vindo(a), é um prazer ter você aqui! 🎉</h1>
+            <p>Agora você faz parte da nossa família pet, onde oferecemos os melhores serviços para cuidar do seu bichinho com carinho e dedicação. 🐶🐱✨</p>
+            <h2>Agende já um dos nossos serviços para seu Pet</h2>
+            <button className="botao">AGENDAR</button>
+          </div>
+        </div>
       </div>
-    
-  )
+
+      <footer>
+        &copy; 2025 PetShop. Todos os direitos reservados.
+      </footer>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
